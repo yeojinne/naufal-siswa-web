9786dfa8dbd60a8a82987a809d428ec107199fe5
aryafuri <167596530+yeojinne@users.noreply.github.com>
2024-06-11 20:46:29 +0700

    Add files via upload

--- edit.php
+++ edit.php
@@ -1,34 +1,55 @@
 <div class="panel panel-default">
     <div class="panel-heading">
-        Form Edit Pelanggaran  <span class="pull-right clickable panel-toggle panel-button-tab-left">
-            <em class="fa fa-toggle-up">
-
-            </em></span></div>
+        FORM  EDIT PENILAIAN
+        <span class="pull-right clickable panel-toggle panel-button-tab-left"><em class="fa fa-toggle-up"></em></span></div>
     <div class="panel-body">
-        <?php
-        echo form_open('Admin/Pelanggaran/edit', 'class="form-horizontal"');
-        echo form_hidden('Id', $pelanggaran['Id']);
-        ?>
+        <?php echo form_open('Admin/Penilaian/edit', 'class="form-horizontal"');
+              echo form_hidden('id_nilai',$nilai['id_nilai']) ;       
+         ?>
         <fieldset>
             <!-- Name input-->
             <div class="form-group">
-                <label class="col-md-3 control-label" for="nama">Nama</label>
+                <label class="col-md-3 control-label" for="name">Name</label>
                 <div class="col-md-9">
-                    <input id="email" name="nama" value="<?php echo $pelanggaran['nama'] ?>" type="text" placeholder="nama" class="form-control">
+                    <?php echo cmb_dinamis('nis', 'siswa', 'nama', 'nis',$nilai['nama'], "id='nis' onChange='isi_otomatis()'"); ?>
                 </div>
             </div>
 
+            <!-- Email input-->
             <div class="form-group">
-                <label class="col-md-3 control-label" for="alamat">Bobot</label>
+                <label class="col-md-3 control-label" for="email">Bobot Pelanggaran</label>
                 <div class="col-md-9">
-                    <input id="email" name="bobot" value="<?php echo $pelanggaran['bobot'] ?>" type="text" placeholder="bobot" class="form-control">
+                    <?php echo cmb_dinamis('id_pelanggaran', 'pelanggaran', 'bobot','Id',$nilai['bobot']) ?>
                 </div>
             </div>
 
+
+
             <div class="form-group">
-                <div class="col-md-3 widget-right">
-                    <button type="submit" name="submit" class="btn btn-info btn-md ">Submit</button>
-                    <?php echo anchor('Admin/Pelanggaran', 'KEMBALI', array('class' => "btn btn-danger btn-md pull-righ")) ?>
+                <label class="col-md-3 control-label" for="message">Tanggal</label>
+                <div class="col-md-9">
+                    <input type="date" name="tanggal" value="<?php echo $nilai['tanggal'] ?>" class="form-control">
+                </div>
+            </div>
+
+            <!-- Message body -->
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="message">Keterangan</label>
+                <div class="col-md-9">
+                    <textarea class="texteditor" id="message" name="keterangan" placeholder="Please enter your message here..." rows="5"><?php echo $nilai['keterangan'] ?></textarea>
+                </div>
+            </div>
+
+
+
+
+            <!-- Form actions -->
+            <div class="form-group">
+                <div class="col-md-12 widget-right">
+                    <center>
+                        <button type="submit" name="submit" class="btn btn-danger btn-md">simpam</button>
+                        <?php echo anchor('Admin/Penilaian', 'kembali', array('class' => "btn btn-info btn-md pull-righ")) ?>
+                    </center>
                 </div>
             </div>
         </fieldset>
