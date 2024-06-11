665b791550747a5cb9d1b4b7e4aa0f9e3d0e6600
aryafuri <167596530+yeojinne@users.noreply.github.com>
2024-06-11 20:53:14 +0700

    Add files via upload

--- edit.php
+++ edit.php
@@ -1,50 +1,58 @@
 <div class="panel panel-default">
     <div class="panel-heading">
-        Form Edit siswa  <span class="pull-right clickable panel-toggle panel-button-tab-left">
-            <em class="fa fa-toggle-up">
-
-            </em></span></div>
+        FORM  EDIT PENILAIAN
+        <span class="pull-right clickable panel-toggle panel-button-tab-left"><em class="fa fa-toggle-up"></em></span></div>
     <div class="panel-body">
-        <?php
-        echo form_open('Admin/Siswa/edit', 'class="form-horizontal"');
-        echo form_hidden('nis', $siswa['nis']);
-        ?>
+        <?php echo form_open('Admin/Penilaian/edit', 'class="form-horizontal"');
+              echo form_hidden('id_nilai',$nilai['id_nilai']) ;       
+         ?>
         <fieldset>
             <!-- Name input-->
             <div class="form-group">
-                <label class="col-md-3 control-label" for="nama">Nama</label>
+                <label class="col-md-3 control-label" for="name">Name</label>
                 <div class="col-md-9">
-                    <input id="email" name="nama" value="<?php echo $siswa['nama'] ?>" type="text" placeholder="nama" class="form-control">
+                    <?php echo cmb_dinamis('nis', 'siswa', 'nama', 'nis',$nilai['nama'], "id='nis' onChange='isi_otomatis()'"); ?>
                 </div>
             </div>
+
+            <!-- Email input-->
+            <div class="form-group">
+                <label class="col-md-3 control-label" for="email">Bobot Pelanggaran</label>
+                <div class="col-md-9">
+                    <?php echo cmb_dinamis('id_pelanggaran', 'pelanggaran', 'bobot','Id',$nilai['bobot']) ?>
+                </div>
+            </div>
+
+
 
             <div class="form-group">
-                <label class="col-md-3 control-label" for="alamat">Alamat</label>
+                <label class="col-md-3 control-label" for="message">Tanggal</label>
                 <div class="col-md-9">
-                    <input id="email" name="alamat" value="<?php echo $siswa['alamat'] ?>" type="text" placeholder="alamat" class="form-control">
+                    <input type="date" name="tanggal" value="<?php echo $nilai['tanggal'] ?>" class="form-control">
                 </div>
             </div>
 
+            <!-- Message body -->
             <div class="form-group">
-                <label class="col-md-3 control-label" for="walikelas">Wali kelas</label>
+                <label class="col-md-3 control-label" for="message">Keterangan</label>
                 <div class="col-md-9">
-                    <?php echo cmb_dinamis('nik', 'guru', 'nama', 'nik', $siswa['nis']); ?>
+                    <textarea class="texteditor" id="message" name="keterangan" placeholder="Please enter your message here..." rows="5"><?php echo $nilai['keterangan'] ?></textarea>
                 </div>
             </div>
 
-            <div class="form-group">
-                <label class="col-md-3 control-label" for="nohp">No hp orang tua</label>
-                <div class="col-md-9">
-                    <input id="email" name="no_hp_ortu" value="<?php $siswa['no_hp_ortu'] ?>" type="text" placeholder="no_hp" class="form-control">
-                </div>
-            </div>
 
+
+
+            <!-- Form actions -->
             <div class="form-group">
                 <div class="col-md-12 widget-right">
-                    <button type="submit" name="submit" class="btn btn-info btn-md ">Submit</button>
-                    <?php echo anchor('Admin/Siswa', 'KEMBALI', array('class' => "btn btn-danger btn-md pull-righ")) ?>
+                    <center>
+                        <button type="submit" name="submit" class="btn btn-danger btn-md">simpam</button>
+                        <?php echo anchor('Admin/Penilaian', 'kembali', array('class' => "btn btn-info btn-md pull-righ")) ?>
+                    </center>
                 </div>
             </div>
         </fieldset>
         <?php echo form_close(); ?>
     </div>
+</div>
